455b5008854f988aa20cb8e3e04aa200a7f97791
mabzinesspatrick360 <bsc-com-ne-10-18@cc.ac.mw>
2021-07-04 02:29:33 +0200

    after adding the functionality of pinging and getting the ip address

--- src/sample/HomeController.java
+++ src/sample/HomeController.java
@@ -2,10 +2,11 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-
-import java.awt.event.ActionEvent;
+import javafx.scene.control.TextField;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.URL;
@@ -13,9 +14,14 @@ import java.net.UnknownHostException;
 import java.util.ResourceBundle;
 
 public class HomeController implements Initializable {
-    @FXML public Label LBconnectionStatus;
-    @FXML public Label LBpublicIP;
-    @FXML public Label LBprivateip;
+    @FXML public Label LBconnectionStatus =new Label();
+    @FXML public Label LBpublicIP =new Label();
+    @FXML public Label LBprivateip = new Label();
+    @FXML public TextField TFnewHost = new TextField();
+    @FXML public Button BtnGo = new Button();
+    @FXML public Label TFPingingReport = new Label();
+    @FXML public Label LBNewHostStatus1 = new Label();
+    @FXML public Label hostnamelb =new Label();
     String publicipaddress = "";
     String privateip;
 //the object of the main class
@@ -28,6 +34,28 @@ public class HomeController implements Initializable {
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //continous pinging
+        int i=1;
+        //for(int x=2;x!=i;x--){
+            String ipAddress ="8.8.8.8";
+            InetAddress hostAddress = null;
+            try {
+                hostAddress = InetAddress.getByName(ipAddress);
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
+            //TFPingingReport.setText("Sending Ping Request to " + ipAddress);
+            try {
+                if (hostAddress.isReachable(5000))
+                    LBconnectionStatus.setText("CONNECTED");
+                else
+                    LBconnectionStatus.setText("NOT CONNECTED");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        //}
+
     }
     public void connectionDetails(){
         main.loadStage("ConnectionDetails.fxml");
@@ -46,6 +74,9 @@ public class HomeController implements Initializable {
         // Returns the instance of InetAddress containing local host name and address
         InetAddress localhost = InetAddress.getLocalHost();
         privateip=localhost.getHostAddress().trim();
+
+        String hostName=localhost.getHostName();
+        hostnamelb.setText(hostName);
         //setting the private ip to the label
         LBprivateip.setText(""+privateip);
         // Find public IP address
@@ -67,4 +98,26 @@ public class HomeController implements Initializable {
 
         return  privateip;
     }
+
+    //the pinging method
+    public void pinging() throws UnknownHostException {
+        String ipAddress =TFnewHost.getText();
+        InetAddress hostAddress = null;
+        try {
+            hostAddress = InetAddress.getByName(ipAddress);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        TFPingingReport.setText("Sending Ping Request to " + ipAddress);
+        try {
+            if (hostAddress.isReachable(5000))
+                LBNewHostStatus1.setText("Host is reachable");
+            else
+                LBNewHostStatus1.setText("Sorry ! host not reachable");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,6 +7,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Enumeration;
+
+import static java.lang.System.out;
 
 public class Main extends Application {
     Stage stg =new Stage();
@@ -30,7 +38,24 @@ public class Main extends Application {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SocketException {
+        Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+        for (NetworkInterface netint : Collections.list(nets)) {
+            displayInterFaceInformation(netint);
+        }
         launch(args);
     }
+
+     static void displayInterFaceInformation(NetworkInterface netint) throws SocketException{
+        out.printf("display name : %s\n" , netint.getDisplayName());
+        out.printf("name :%s\n", netint.getName());
+        Enumeration<InetAddress> inetAddresses =netint.getInetAddresses();
+        for(InetAddress inetAddress : Collections.list(inetAddresses)){
+            out.printf("inetAddress %s\n", inetAddress );
+        }
+         out.printf("up? :%s\n", netint.isUp());
+         out.printf("virtual? :%s\n", netint.isVirtual());
+        out.printf("\n");
+    }
+
 }
